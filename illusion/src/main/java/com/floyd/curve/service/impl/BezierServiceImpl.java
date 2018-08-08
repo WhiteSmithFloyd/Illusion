32ffc9ea7e6e949d2ea18bae8e1f411db464b316
WhiteSmithFloyd <eurika_think@163.com>
2018-08-08 22:12:43 +0800

    animation

--- illusion/src/main/java/com/floyd/curve/service/impl/BezierServiceImpl.java
+++ illusion/src/main/java/com/floyd/curve/service/impl/BezierServiceImpl.java
@@ -23,87 +23,136 @@ public class BezierServiceImpl implements ICurveService {
 
 	/** 
 	 * convert to Bezier from string of normal Point
+	 * @throws Exception 
 	 */
-	public String draw4Point(String pointSrc) {
-		try {
-			
-			if(StringUtils.isEmpty(pointSrc)) {
-				throw new Exception("Point could NOT be empty..");
-			}
-			// parse the string inputed and convert to PointBean
-			List<PointBean> ppList = PointUtils.fetchPoint(pointSrc);
-			
-			return pointToBezier(ppList);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return e.getMessage();
+	public String draw4Point(String pointSrc) throws Exception {
+		if(StringUtils.isEmpty(pointSrc)) {
+			throw new Exception("Point could NOT be empty..");
 		}
+		
+		// parse the string inputed and convert to PointBean
+		List<PointBean> ppList = PointUtils.fetchPoint(pointSrc);
+		
+		return pointToBezierToStr(ppList);
 	}
 	
 	/** 
 	 * convert to Bezier from string of Circle
+	 * @throws Exception 
 	 */
-	public String draw4Circle(String circleSrc)  {
-		try {
-			if(StringUtils.isEmpty(circleSrc)) {
-					throw new Exception("Circle could NOT be empty..");
-			}
-			
-			CircleBean circle = CurveUtils.fetchCircle(circleSrc);		
-			CircleBezierConverter convert = new CircleBezierConverter();
-			convert.setCircle(circle);
-			convert.convertToBezier();
-			
-			return pointToBezier(circle.getPointList());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return e.getMessage();
+	public String draw4Circle(String circleSrc) throws Exception  {
+		if(StringUtils.isEmpty(circleSrc)) {
+				throw new Exception("Circle could NOT be empty..");
 		}
+		
+		CircleBean circle = CurveUtils.fetchCircle(circleSrc);		
+		CircleBezierConverter convert = new CircleBezierConverter();
+		convert.setCircle(circle);
+		convert.convertToBezier();
+		
+		return pointToBezierToStr(circle.getPointList());
 	}
 	
 	/**
 	 * convert to Bezier from string of Rect
+	 * @throws Exception 
 	 */
-	public String draw4Rect(String rectStr) {
-		try {
-			if(StringUtils.isEmpty(rectStr)) {
-				throw new Exception("Rect String could NOT be empty..");
-			}
-			
-			RectBean rect = CurveUtils.fetchRect(rectStr);		
-			RectBezierConverter convert = new RectBezierConverter();
-			convert.setRect(rect);
-			convert.convertToBezier();
-			
-			return pointToBezier(rect.getPointList());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return e.getMessage();
+	public String draw4Rect(String rectStr) throws Exception {
+		if(StringUtils.isEmpty(rectStr)) {
+			throw new Exception("Rect String could NOT be empty..");
 		}
+		
+		RectBean rect = CurveUtils.fetchRect(rectStr);		
+		RectBezierConverter convert = new RectBezierConverter();
+		convert.setRect(rect);
+		convert.convertToBezier();
+		
+		return pointToBezierToStr(rect.getPointList());
 	}
 	
 	
 	/**
 	 * convert to Bezier from string of Polygon
+	 * @throws Exception 
 	 */
-	public String draw4Polygon(String polyStr) {
-		try {
-			if(StringUtils.isEmpty(polyStr)) {
-				throw new Exception("Polygon String could NOT be empty..");
+	public String draw4Polygon(String polyStr) throws Exception {
+		if(StringUtils.isEmpty(polyStr)) {
+			throw new Exception("Polygon String could NOT be empty..");
+		}
+		
+		PolygonBean poly = new PolygonBean();	
+		poly.setOriginStr(polyStr);
+		
+		PolygonBezierConverter convert = new PolygonBezierConverter();
+		convert.setPoly(poly);
+		convert.convertToBezier();
+		
+		return pointToBezierToStr(poly.getPointList());
+	}
+	
+	/**
+	 * Animation of transfer, return JSON String
+	 * @throws Exception 
+	 */
+	public String animate4Transfer(String pStrFrom, String pStrTo) throws Exception {
+		if(StringUtils.isEmpty(pStrFrom)) {
+				throw new Exception("[The From Curvse] String could NOT be empty..");
+		}
+		if(StringUtils.isEmpty(pStrTo)) {
+			throw new Exception("[The To Curvse] String could NOT be empty..");
+		}
+		
+		// parse the string inputed and convert to PointBean
+		List<PointBean> ppFromList = PointUtils.fetchPoint(pStrFrom);
+		ppFromList = pointToBezier(ppFromList);
+		
+		List<PointBean> ppToList = PointUtils.fetchPoint(pStrTo);
+		ppToList = pointToBezier(ppToList);
+		
+		int fromSize = ppFromList.size();
+		int toSize = ppToList.size();
+		
+		if(fromSize ==0 || toSize ==0) {
+			throw new Exception("From Point or To Point bizier changing Exception");
+		}
+		
+		int ft = fromSize - toSize;
+		if(ft == 0) {
+			return "{bFromPoint:{bData: " +pointListToStr(ppFromList)+", length:" + fromSize + "}"
+					+", bToPoint:{bData: " +pointListToStr(ppToList)+ ", length:" + toSize +"}}";
+		}
+		
+		if(ft > 0) {
+			ppFromList = fillup(ppFromList, ft);
+		} else {
+			ppToList = fillup(ppToList, -ft);
+		}
+		
+		return "{bFromPoint:{bData: " +pointListToStr(ppFromList)+", length:" + fromSize + "}"
+			+", bToPoint:{bData: " +pointListToStr(ppToList)+ ", length:" + toSize +"}}";
+	}
+	
+	/**
+	 * Fill Up Point for animation.
+	 * @param ppList
+	 * @param length
+	 * @return
+	 */
+	private List<PointBean> fillup(List<PointBean> ppList, int length) {
+		List<PointBean> rsList = new ArrayList<PointBean>();
+		
+		for(int i=0; i<ppList.size(); i++) {
+			if(i==0) {
+				rsList.add(ppList.get(i));
+				continue;
 			}
 			
-			PolygonBean poly = new PolygonBean();	
-			poly.setOriginStr(polyStr);
-			
-			PolygonBezierConverter convert = new PolygonBezierConverter();
-			convert.setPoly(poly);
-			convert.convertToBezier();
-			
-			return pointToBezier(poly.getPointList());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return e.getMessage();
+			for(int count=0; count<length; count ++) {
+				rsList.add(new PointBean());
+			}
+			rsList.add(ppList.get(i));
 		}
+		return rsList;
 	}
 	
 	/**
@@ -112,11 +161,11 @@ public class BezierServiceImpl implements ICurveService {
 	 * @return
 	 * @throws Exception
 	 */
-	private String pointToBezier(List<PointBean> ppList) throws Exception {
+	private List<PointBean> pointToBezier(List<PointBean> ppList) throws Exception {
 		
 		PointBezierConverter convert = new PointBezierConverter();
 		if(ppList==null || ppList.size()==0) {
-			return "Wrong! Calculation Has Exception!";
+			throw new Exception("Wrong! Calculation Has Exception!");
 		}
 		
 		PointBean virtualPoint = firstP2c();
@@ -145,7 +194,17 @@ public class BezierServiceImpl implements ICurveService {
 			convert.launch(ppList.get(i), i==1 ? virtualPoint : ppList.get(i-1));
 		}
 		
-		return pointListToStr(ppList);
+		return ppList;
+	}
+	
+	/**
+	 * Convert point to Bezier
+	 * @param pointSrc
+	 * @return
+	 * @throws Exception
+	 */
+	private String pointToBezierToStr(List<PointBean> ppList) throws Exception {
+		return pointListToStr(pointToBezier(ppList));
 	}
 	
 	/**
