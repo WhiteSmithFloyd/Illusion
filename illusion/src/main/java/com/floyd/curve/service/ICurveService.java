fae607ca95c3fd4c2955958c219929d62783db8a
WhiteSmithFloyd <eurika_think@163.com>
2018-08-08 22:12:57 +0800

    animation

--- illusion/src/main/java/com/floyd/curve/service/ICurveService.java
+++ illusion/src/main/java/com/floyd/curve/service/ICurveService.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ICurveService {
 
-	public String draw4Point(String src);
+	public String draw4Point(String src) throws Exception;
 	
-	public String draw4Circle(String src);
+	public String draw4Circle(String src) throws Exception;
 	
-	public String draw4Rect(String src);
+	public String draw4Rect(String src) throws Exception;
 	
-	public String draw4Polygon(String src);
+	public String draw4Polygon(String src) throws Exception;
 	
+	public String animate4Transfer(String pStrFrom, String pStrTo) throws Exception;
+
 }
